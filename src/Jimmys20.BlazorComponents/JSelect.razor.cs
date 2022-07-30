71d8f511f1b8c31a367f74ffa16a521795e40437
jimmys20 <maragkosd12@gmail.com>
2022-07-30 04:53:04 +0300

    commit

---
+++ src/Jimmys20.BlazorComponents/JSelect.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Jimmys20.BlazorComponents
+{
+    public partial class JSelect<T>
+    {
+        [Parameter]
+        public IEnumerable<T> Data { get; set; }
+        [Parameter]
+        public string DisplayItem { get; set; }
+        [Parameter]
+        public string ValueItem { get; set; }
+
+        private bool isVisible { get; set; }
+    }
+}
