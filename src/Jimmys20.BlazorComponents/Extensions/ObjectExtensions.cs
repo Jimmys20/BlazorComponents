df9c7cf5ef6efa40a94a7efa42fd799b405999f4
jimmys20 <maragkosd12@gmail.com>
2022-08-14 02:34:45 +0300

    target net5.0

---
+++ src/Jimmys20.BlazorComponents/Extensions/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Jimmys20.BlazorComponents.Extensions
+{
+    internal static class ObjectExtensions
+    {
+        public static object GetPropertyValue(this object obj, string propertyName)
+        {
+            var type = obj.GetType();
+            var property = type.GetProperty(propertyName);
+            var value = property.GetValue(obj, null);
+
+            return value;
+        }
+    }
+}
