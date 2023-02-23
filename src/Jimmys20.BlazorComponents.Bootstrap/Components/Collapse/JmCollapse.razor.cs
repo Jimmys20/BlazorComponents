61f59d280bc2ff2a082e35e99e7073762bf15b78
Dimitrios Maragkos <34100964+Jimmys20@users.noreply.github.com>
2023-02-23 02:28:22 +0200

    collapse

---
+++ src/Jimmys20.BlazorComponents.Bootstrap/Components/Collapse/JmCollapse.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazorComponentUtilities;
+using Microsoft.AspNetCore.Components;
+
+namespace Jimmys20.BlazorComponents.Bootstrap
+{
+    public partial class JmCollapse : BootstrapComponentBase
+    {
+        /// <summary>
+        /// Specifies if the collapse is visible.
+        /// </summary>
+        [Parameter] public bool Visible { get; set; }
+
+        /// <summary>
+        /// Specifies the content of the collapse.
+        /// </summary>
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        protected override CssBuilder CssBuilder => base.CssBuilder
+            .AddClass("collapse")
+            .AddClass("show", when: Visible);
+    }
+}
