c6f623e863fef374641d077cd6efcb382e27c4be
jimmys20 <maragkosd12@gmail.com>
2023-03-03 15:13:32 +0200

    Use library components in demo

--- src/Jimmys20.BlazorComponents.Bootstrap/Components/Card/JmCardHeader.razor.cs
+++ src/Jimmys20.BlazorComponents.Bootstrap/Components/Card/JmCardHeader.razor.cs
@@ -6,11 +6,17 @@ namespace Jimmys20.BlazorComponents.Bootstrap
     public partial class JmCardHeader : BootstrapComponentBase
     {
         /// <summary>
+        /// Specifies the color of the card header.
+        /// </summary>
+        [Parameter] public Color Color { get; set; }
+
+        /// <summary>
         /// Specifies the content of the card header.
         /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
 
         protected override CssBuilder CssBuilder => base.CssBuilder
-            .AddClass("card-header");
+            .AddClass("card-header")
+            .AddClass($"text-bg-{Color.ToColorString()}", when: Color != Color.Default);
     }
 }
